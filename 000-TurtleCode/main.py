d127de0505cb51404d0e52f6713f2cae4b0ff187
Mr-Turtle <62259727+Mr-Turtle@users.noreply.github.com>
2021-03-09 11:55:20 +0000

    Add files via upload

---
+++ 000-TurtleCode/main.py
@@ -0,0 +1,79 @@
+from tkinter import *
+import scripts.version as version
+import scripts.conversions as conversions
+import json
+
+
+
+class TurtleCode:
+
+    def __init__(self):
+
+        # Files
+        self.config = json.load(open("config.json"))
+        self.lang = json.load(open(self.config["default_lang_path"]))
+
+        # Variable declaration START
+        self.file_text = ""
+        self.default_font = (self.config["default_font"], self.config["default_font_size"])
+
+        self.version_num = int(version.get_version_num())
+
+        self.window = Tk()
+        self.main_text_box = Text(self.window, font=self.default_font)
+        # Variable declaration END
+
+        self.window_setup()
+        self.bindings()
+        self.pack_widgets()
+        self.window.mainloop()
+
+    def window_setup(self):
+
+        self.window.config(bg=self.config["window"]["bg_col"])
+        self.window.geometry(self.config["window"]["size"])
+        self.window.title(self.config["window"]["title"].replace("{VERSION}", str(self.version_num)))
+
+    def pack_widgets(self):
+
+        self.main_text_box.pack(expand=1, fill=BOTH)
+
+    def bindings(self):
+
+        self.main_text_box.bind("<KeyRelease>", self.text_callback)
+
+    def text_callback(self, event):
+        self.file_text = self.main_text_box.get("1.0", END)
+
+        for tag in self.main_text_box.tag_names():
+            self.main_text_box.tag_delete(tag)
+
+        self.get_tags()
+
+    def get_tags(self):
+
+        split_lines = self.file_text.split("\n")
+        for line_num in range(len(split_lines)):
+            word_split = split_lines[line_num].split()
+            for word_num in range(len(word_split)):
+                start_ind = conversions.wordNum_charIndex(split_lines[line_num], word_num)
+                start = f"{str(line_num+1)}.{start_ind}"
+                end = f"{str(line_num+1)}.{start_ind+len(word_split[word_num])+1}"
+                syntax = self.get_word_syntax(word_split[word_num])
+                col = syntax["col"]
+                style = syntax["style"]
+
+                if style is not None: font = (self.config["default_font"], self.config["default_font_size"], style)
+                else: font = self.default_font
+                self.main_text_box.tag_add(word_split[word_num]+start, start, end)
+                self.main_text_box.tag_config(word_split[word_num]+start, foreground=col, font=font)
+
+    def get_word_syntax(self, word: str):
+
+        for syntax_word in self.lang["syntax_highlighting"]:
+            if syntax_word["word"] == word:
+                return syntax_word
+        return {"col":self.config["default_word_col"], "style":None}
+
+
+t = TurtleCode()
